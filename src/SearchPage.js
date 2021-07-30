222404b384e28ca4b6c56d9feef7b2369501813d
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2021-07-31 00:34:35 +0100

    chore: Uninstall lodash.debounce

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 const SearchPage = () => {
   const [queryParam, setQueryParam] = useState("");
 
   const handleChange = (e) => {
-    setQueryParam(() => e.target.value);
+    setQueryParam(e.target.value);
     console.log(queryParam);
   };
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
